ebebe5ed316bbd6aa4e20412df8f94d80edcd27a
Matela-cyber <143181642+Matela-cyber@users.noreply.github.com>
2024-03-06 17:32:16 +0200

    app

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, Image, Text } from "react-native";
+import Stylee from "./stylee";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.profiles}>
+        <View style={styles.profile}>
+          <Image source={require("./profile.png")} style={styles.image} />
+          <Stylee label="Thabo Lebese" value="10hr" />
+        </View>
+        <View style={styles.topRight}>
+          <Text>...</Text>
+        </View>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "start",
+    padding: 20,
+  },
+  image: {
+    height: 40,
+    width: 40,
+    borderRadius: 50,
+  },
+  profile: { flexDirection: "row" },
+  profiles: { flexDirection: "row", justifyContent: "space-between" },
+  topRight: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    paddingTop: 10,
+    paddingRight: 10,
   },
 });
