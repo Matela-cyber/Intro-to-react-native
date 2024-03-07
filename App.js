9d87125a0090da9c3921665135486fe534fd15cc
Matela-cyber <143181642+Matela-cyber@users.noreply.github.com>
2024-03-07 05:12:11 +0200

    app first changes

--- App.js
+++ App.js
@@ -1,44 +1,84 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, Text } from "react-native";
-import Stylee from "./stylee";
+import React from "react";
+import { StatusBar, Image, StyleSheet, Text, View } from "react-native";
+import {
+  AntDesign,
+  FontAwesome,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.profiles}>
-        <View style={styles.profile}>
-          <Image source={require("./profile.png")} style={styles.image} />
-          <Stylee label="Thabo Lebese" value="10hr" />
+      <View style={styles.top}>
+        <View style={styles.leftcon}>
+          <Image
+            source={require("./profile.png")}
+            style={styles.profilepicture}
+          />
+          <View style={styles.text}>
+            <Text>Teboho Matela</Text>
+            <Text>10hrs</Text>
+          </View>
         </View>
-        <View style={styles.topRight}>
-          <Text>...</Text>
-        </View>
+        <Text>...</Text>
       </View>
 
+      <Text>
+        Software development is a complex process that involves various stages,
+        methodologies, and models to ensure the successful delivery of
+        high-quality software products. The software life cycle models provide a
+        structured approach for the development, deployment, and maintenance of
+        software systems.
+      </Text>
+      <Image source={require("./copy.png")} style={styles.copy} />
+      <Text>62 likes</Text>
+      <View style={styles.bottom}>
+        <View>
+          <AntDesign name="like1" size={24} color="black" />
+          <Text>Likes</Text>
+        </View>
+        <View>
+          <FontAwesome name="comment-o" size={24} color="black" />
+          <Text>Comment</Text>
+        </View>
+        <View>
+          <MaterialCommunityIcons
+            name="share-outline"
+            size={24}
+            color="black"
+          />
+          <Text>Share</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "start",
-    padding: 20,
-  },
-  image: {
-    height: 40,
+  profilepicture: {
+    padding: 10,
     width: 40,
+    height: 40,
     borderRadius: 50,
   },
-  profile: { flexDirection: "row" },
-  profiles: { flexDirection: "row", justifyContent: "space-between" },
-  topRight: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    paddingTop: 10,
-    paddingRight: 10,
+  copy: {
+    width: 400,
+    height: 400,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  top: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  leftcon: {
+    flexDirection: "row",
+  },
+  bottom: {
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
